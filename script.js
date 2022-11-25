f4b44807f0b1cd5a9074897f59860bff5b39e33a
7Kamil <115102676+7Kamil@users.noreply.github.com>
2022-11-25 09:49:53 +0400

    script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const filcon=document.querySelector('.filter_icon');
+const tblock=document.querySelector('.tblock');
+const add_button=document.querySelector('.add_button');
+let tadd = document.querySelector(".text");
+let play = document.querySelector('.text_input');   
+let delete_button = document.querySelector('.delete_button');
+let k=0;
+function sort_inputs(a,b,d,u){
+let tsort = [...tblock.children]; 
+tblock.innerHTML = "";
+tsort=tsort.sort((c,d) => c.querySelector("input").value > d.querySelector("input").value ? a : b);
+tblock.append(...tsort);
+filcon.classList.replace(d,u);
+}
+add_button.addEventListener("click", () => {
+tadd = document.createElement("div");
+tadd.setAttribute('class', 'text');
+play = document.createElement('input');
+play.type = "text";
+play.setAttribute('class', 'text_input');
+delete_button = document.createElement('div');
+delete_button.setAttribute('class', 'delete_button');
+delete_button.addEventListener("click", (event) => {
+event.target.closest(".text").remove();
+});
+tadd.append(play, delete_button);
+tblock.append(tadd);
+});
+delete_button.addEventListener("click", (event) => {
+event.target.closest(".text").remove();
+});
+filcon.addEventListener("click", () => {
+let a="up";
+let b="down";
+k=k+1;
+if(k%2==1){
+inputs(1,-1,b,a);
+}
+else{
+inputs(-1,1,a,b);
+}
+});
